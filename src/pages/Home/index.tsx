91bb5a29599c437cb2e6a91cd647a3191fa4a1b3
gutodiasux <gutodiasux@gmail.com>
2021-10-31 13:08:50 -0300

    Build

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,46 @@
 import './styles.scss';
+import { FormEvent, useState } from 'react';
+import axios from 'axios';
 
 export function Home() {
+
+  const [endDateForm, setEndDateForm] = useState('');
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (endDateForm.trim() === '') {
+      return;
+    }
+
+    const data = {
+      endDate: `${endDateForm}-03:00`,
+      fields: ["https://call-meethub.whereby.com/"],
+    }
+
+    const headers = {
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+      "Content-Type": "application/json",
+    }
+
+    axios.post("https://api.whereby.dev/v1/meetings", data, { headers })
+      .then(response => {
+        console.log(response.data);
+      });
+  }
+
   return (
     <div className="main">
       <div className="content">
         <label htmlFor="endDate">Data de término</label>
-        <form>
+        <form onSubmit={handleCreateRoom}>
           <input
             type="datetime-local"
             name="endDate"
             id="endDate"
             placeholder="Data e hora de término do meet"
+            onChange={event => setEndDateForm(event.target.value)}
+            value={endDateForm}
           />
           <button type="submit">Criar sala</button>
         </form>
