df67630e5774387ad6e8f0503bc3cb2b4caaeb62
Augusto Dias <gutodiasux@gmail.com>
2022-05-31 14:48:46 -0300

    Update api.ts
    insert herokuapp.com

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://wherebyserver.vercel.app/",
+  baseURL: "https://wherebyserver.herokuapp.com/",
 })
 
 export default api;
